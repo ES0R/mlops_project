a9613c26868667497cd45aa172e5bb25fd49b94d
Andrew <hujo888@gmail.com>
2024-01-08 23:17:26 +0100

    prediction update

--- src/predict_model.py
+++ src/predict_model.py
@@ -24,7 +24,7 @@ transform = transforms.Compose([
 label_encoder = torch.load('data/processed/label_encoder.pt')
 
 # Load and preprocess new images
-new_image_paths = ["data/raw/images_dog_small/n02105412-kelpie/n02105412_123.jpg"]  # Replace with actual file paths
+new_image_paths = ["data/raw/images_dog_small/n02110958-pug/n02110958_353.jpg", "data/raw/images_dog_small/n02110185-Siberian_husky/n02110185_699.jpg"]  # Replace with actual file paths
 new_images = [transform(Image.open(path)).to(device) for path in new_image_paths]
 
 # Run inference on new images
@@ -32,13 +32,18 @@ model.eval()
 with torch.no_grad():
     for i, new_image in enumerate(new_images):
         output = model(new_image.unsqueeze(0))  # Add batch dimension
-        _, predicted_class = torch.max(output, 1)
-        
-        # Get the predicted class label from the label encoder
-        predicted_label = label_encoder.inverse_transform([predicted_class.item()])[0]
-        
-        # Plot the image and predicted class
+        probabilities = torch.nn.functional.softmax(output[0], dim=0)
+        top5_probabilities, top5_classes = torch.topk(probabilities, 5)
+
+        # Get the predicted class labels from the label encoder
+        predicted_labels = label_encoder.inverse_transform(top5_classes.cpu().numpy())
+
+        # Plot the image and predicted class with top 5 probabilities
         image = transforms.ToPILImage()(new_image.cpu()).convert("RGB")
         plt.imshow(image)
-        plt.title(f"Prediction for image {i+1}: Class {predicted_label}")
+        plt.title(f"Prediction for image {i + 1}: Top 5 Classes and Probabilities")
+        plt.axis('off')
+
         plt.show()
+        print(predicted_labels)
+        print(top5_probabilities)
